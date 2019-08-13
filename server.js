f3fb44bd2975da0293c47641fec179a62bdac63b
Brian Nelson <nelsonsbrian@gmail.com>
2019-08-13 09:57:14 -0700

    add initial file structure and setup first api call

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.json({ message: 'Send the data down!!' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
