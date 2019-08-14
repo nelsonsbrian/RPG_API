3eeddb598047d26943830507f6bc6f9f6e18dd87
Brian Nelson <nelsonsbrian@gmail.com>
2019-08-14 11:17:06 -0700

    add chracter update and delete. Add defaults for character properties.

--- app/models/player.js
+++ app/models/player.js
@@ -1,8 +1,69 @@
 const mongoose = require('mongoose');
-var Schema       = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 const PlayerSchema = new Schema({
-  name: String
+  name: String, level: Number,
+  inventory: Array,
+  gold: Number,
+  updated: { type: Date, default: Date.now },
+  attributes: {
+    health: {
+      delta: {
+        type: Number,
+      },
+      base: {
+        type: Number
+      }
+    },
+    mana: {
+      delta: {
+        type: Number,
+      },
+      base: {
+        type: Number
+      }
+    },
+    strength: {
+      delta: {
+        type: Number,
+      },
+      base: {
+        type: Number
+      }
+    },
+    hitroll: {
+      delta: {
+        type: Number,
+      },
+      base: {
+        type: Number
+      }
+    },
+    damroll: {
+      delta: {
+        type: Number,
+      },
+      base: {
+        type: Number
+      }
+    }
+  }
 });
+
+PlayerSchema.methods.setDefaults = function() {
+  this.level = 1;
+  this.attributes.health.base = 100;
+  this.attributes.health.delta = 0;
+  this.attributes.mana.base = 100;
+  this.attributes.mana.delta = 0;
+  this.attributes.strength.base = 100;
+  this.attributes.strength.delta = 0;
+  this.attributes.hitroll.base = 100;
+  this.attributes.hitroll.delta = 0;
+  this.attributes.damroll.base = 100;
+  this.attributes.damroll.delta = 0;
+  this.gold = 0;
+  this.inventory = [];
+  }
 
 module.exports = mongoose.model('Player', PlayerSchema);
--- routes/player.js
+++ routes/player.js
@@ -3,16 +3,14 @@ const express = require('express');
 const Player = require('../app/models/Player');
 const router = express.Router();
 
+mongoose.set('useFindAndModify', false); // Deprecation warning override
+
 router.get('/', async (req, res) => {
   const players = await Player.find().sort('name');
   res.send(players);
 });
 
 router.get('/:id', async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(404).send('Invalid ID to get.');
-  }
-
   const player = await Player.findById(req.params.id);
 
   if (!player) return res.status(404).send('The player with the given ID was not found');
@@ -21,7 +19,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/create', async (req, res) => {
-  
+
   const error = !req.body.name;
   if (error) return res.status(400).send('Player with no name provided.');
 
@@ -30,16 +28,33 @@ router.post('/create', async (req, res) => {
 
   player = new Player();
   player.name = req.body.name;
+  player.setDefaults();
 
   player.save();
   res.json({ message: "Player created!", player });
 });
 
-router.post('/save', async (req, res) => {
-  const player = new Player();
-  players.push(player);
-  player.name = req.body.name || 'NewPlayer';
-  res.json({ message: "Player created!", player });
+router.put('/:id', async (req, res) => {
+  const player = await Player.findByIdAndUpdate(req.params.id,
+    {
+      name: req.body.name,
+      level: req.body.level,
+      inventory: req.body.inventory
+    },
+    { new: true });
+
+  if (!player) return res.status(404).send('The player with the given ID was not found');
+
+  res.send(player);
+});
+
+router.delete('/:id', async (req, res) => {
+
+  const player = await Player.findByIdAndDelete(req.params.id);
+
+  if (!player) return res.status(404).send('The player with the given ID was not found');
+
+  res.send(player);
 });
 
 module.exports = router;  
